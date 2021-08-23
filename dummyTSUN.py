6eacd565beaff246d318c660aadb221bb4979bd4
Niall Darwin <51384867+NiallDarwin@users.noreply.github.com>
2021-08-23 16:07:48 +1200

    Update dummyTSUN.py

--- dummyTSUN.py
+++ dummyTSUN.py
@@ -199,7 +199,7 @@ msg = cSendMsg( 0x4271, [ LoByte((+100 + ModuleMaxTemp) *10  ), HiByte((+100 + M
 ensemblerspmsg.append(msg)
 
 # Charge/Dis command 
-ChargeForbidden = 0 # 170 (0xAA) for effect
+ChargeForbidden = 170 # 170 (0xAA) for effect
 DischargeForbidden = 170 # 170 (0xAA) for effect
 # 0x428+1 
 msg = cSendMsg( 0x4281, [ LoByte( ChargeForbidden ), LoByte( DischargeForbidden ), 0,0,0,0,0,0 ], 10, 0)
