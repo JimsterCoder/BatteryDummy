14b4fb25ad6f27b76cc90cbc918f14e429506884
Niall Darwin <51384867+NiallDarwin@users.noreply.github.com>
2021-08-23 14:44:52 +1200

    Update dummyTSUN.py

--- dummyTSUN.py
+++ dummyTSUN.py
@@ -172,7 +172,7 @@ ensemblerspmsg.append(msg)
 # Status,Error,Alarm,Protection
 #BasicStatus = initilzied above and modified by PID_SLEEP_AWAKE_COMMAND message
 CyclePeriod = 0 #WTF is this?
-Error = 0 
+Error = 3840 
 Alarm = 0
 Protection = 0
 
